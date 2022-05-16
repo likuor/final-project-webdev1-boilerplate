8ef5865b393c86c0a0fc3a0a73239d68ca2d72e1
Koki Sakai <likuor0528@gmail.com>
2022-05-16 14:21:26 -0700

    Add border countries

--- frontend/static/js/views/CountryView.js
+++ frontend/static/js/views/CountryView.js
@@ -10,19 +10,33 @@ export default class extends AbstractView {
   async getHTML() {
     const countries = getCountries();
     const id = this.params.id;
+    let languages = [];
+    let borders = [];
 
+    //Getting a country data by id
     let countryData = countries.filter((value) => {
       return value['cca3'] === id;
     });
-
     console.log(countryData);
+
+    //Getting language data
+    for (const key in countryData[0].languages) {
+      languages.push(countryData[0].languages[key]);
+    }
+
+    //Getting border data
+    if (countryData[0].borders) {
+      for (let i = 0; i < countryData[0].borders.length; i++) {
+        borders.push(countryData[0].borders[i]);
+      }
+    } else {
+      borders.push('None');
+    }
 
     return `
         <div class='view-container'>
             <div>
-                <button>
-                    back
-                </button>
+                <button>back</button>
             </div>
             <div>
                 <div>
@@ -35,12 +49,14 @@ export default class extends AbstractView {
                     <h2>Region: ${countryData[0].region}</h2>
                     <h2>Sub Region: ${countryData[0].subregion}</h2>
                     <h2>Capital: ${countryData[0].capital}</h2>
+                    <h2>Languages: ${languages}</h2>
+                </div>
+                <div>
+                    <h1>Border Countries</h1>
 
-                    <h2>Languages: ${countryData[0].languages.fra}</h2>
-
+                    <p>${borders}</p>
                 </div>
             </div>
-
         </div>
         `;
   }
